35b452db49b5ecf181ab9402911956fc0bec197b
Andres Gaviria <cgaviria930@gmail.com>
2025-03-08 19:58:10 -0500

    avances navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,13 @@
+import { Link } from "react-router"
+
 export default function Navbar() {
     return(
         <div 
             className="hidden md:flex w-2/4 h-auto justify-between items-center font-semibold text-2xl py-4 px-12 rounded-2xl bg-gray-200 shadow-2xl">
-            <a href="/teams" className="hover:scale-110 hover:transition-transform">EQUIPOS</a>
-            <a href="/" className="hover:scale-110 hover:transition-transform">INICIO</a>
-            <a href="" className="hover:scale-110 hover:transition-transform">GUIAS</a>
-            <a href="" className="hover:scale-110 hover:transition-transform">FARMEO</a>
+            <Link to="/teams" className="hover:scale-110 hover:transition-transform">EQUIPOS</Link>
+            <Link to="/" className="hover:scale-110 hover:transition-transform">INICIO</Link>
+            <Link to="" className="hover:scale-110 hover:transition-transform">GUIAS</Link>
+            <Link to="" className="hover:scale-110 hover:transition-transform">FARMEO</Link>
         </div>
     )
 }

cfb066e23d1e61cf24cbbbb9569c6e11c55fa159
Andres Gaviria <cgaviria930@gmail.com>
2025-03-09 15:47:34 -0500

    final navabr

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router"
 export default function Navbar() {
     return(
         <div 
-            className="fixed top-6 left-1/2 -translate-x-1/2 hidden md:flex w-2/4 h-auto justify-between items-center font-semibold text-2xl py-4 px-12 rounded-xl bg-gray-200 shadow-2xl">
+            className="shadowwhite fixed top-6 left-1/2 -translate-x-1/2 hidden md:flex w-2/4 h-auto justify-between items-center font-semibold text-2xl py-4 px-12 rounded-xl backnavbar shadow-2xl text-white tracking-widest">
             <Link to="/teams" className="hover:scale-110 hover:transition-transform">EQUIPOS</Link>
             <Link to="/" className="hover:scale-110 hover:transition-transform">INICIO</Link>
             <Link to="/Guies" className="hover:scale-110 hover:transition-transform">GUIAS</Link>

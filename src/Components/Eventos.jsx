d1025bbd78cb81a7e139a696668533b7e1f83061
Andres Gaviria <cgaviria930@gmail.com>
2025-03-11 22:14:56 -0500

    avances info tab

--- src/Components/Eventos.jsx
+++ src/Components/Eventos.jsx
@@ -7,11 +7,16 @@ export default function Eventos() {
     return(
         <div>
             <div className="text-center mt-36 flex justify-center gap-x-8">
+                
                 <div className="flex flex-col gap-y-2 bg-red-200">
-                    <button onClick={() => setTab(1)} className="px-4 py-2 bg-blue-600 hover:transition-all hover:scale-110 shadow-[0px_6px_15px_rgba(0,0,0,0.35)] tracking-wider uppercase text-white font-semibold rounded-lg">Taxis</button>
-                    <button onClick={() => setTab(2)} className="px-4 py-2 bg-blue-600 hover:transition-all hover:scale-110 shadow-[0px_6px_15px_rgba(0,0,0,0.35)] tracking-wider uppercase text-white font-semibold rounded-lg">Camionetas</button>
-                    <button onClick={() => setTab(3)} className="px-4 py-2 bg-blue-600 hover:transition-all hover:scale-110 shadow-[0px_6px_15px_rgba(0,0,0,0.35)] tracking-wider uppercase text-white font-semibold rounded-lg">Busetas</button>
+                    <button onClick={() => setTab(1)} className="px-4 py-2 bg-blue-600 hover:transition-all hover:scale-110 shadow-[0px_6px_15px_rgba(0,0,0,0.35)] tracking-wider uppercase text-white font-semibold rounded-lg">
+                        Evento 1</button>
+                    <button onClick={() => setTab(2)} className="px-4 py-2 bg-blue-600 hover:transition-all hover:scale-110 shadow-[0px_6px_15px_rgba(0,0,0,0.35)] tracking-wider uppercase text-white font-semibold rounded-lg">
+                        Evento 2</button>
+                    <button onClick={() => setTab(3)} className="px-4 py-2 bg-blue-600 hover:transition-all hover:scale-110 shadow-[0px_6px_15px_rgba(0,0,0,0.35)] tracking-wider uppercase text-white font-semibold rounded-lg">
+                        Evento 3</button>
                 </div>
+
                 <div className="border border-gray-600 rounded-md bg-blue-200 w-2/3">
                     {tab === 1 && <p className="mx-8 tracking-wide font-semibold">
                         info 1</p>}

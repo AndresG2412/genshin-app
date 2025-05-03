ecf1cd0501be3758e24b32858a403e36ba5a1bab
Andres Gaviria <cgaviria930@gmail.com>
2025-05-02 19:15:54 -0500

    ruta login

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home.jsx'
 import Teams from './pages/Teams.jsx'
 import Guies from './pages/Guies.jsx'
 import Farm from './pages/Farm.jsx'
+import Login from './assets/Login.jsx'
 
 export default function App() {
     return(
@@ -12,6 +13,7 @@ export default function App() {
             <Route path="/farm" element={<Farm />} />
             <Route path="/teams" element={<Teams />} />
             <Route path="/guies" element={<Guies />} />
+            <Route path="/login" element={<Login />} />
         </Routes>
     )
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -17,7 +17,7 @@ export default function Navbar() {
 
             {/* Link/Icono para Login */}
             {/* 2. Corrige el prop 'to' para que use la ruta URL '/login' */}
-            <Link className="" to="../pages/crud/Login.jsx"> {/* Cambiado de "../assets/Login.jsx" a "/login" */}
+            <Link className="" to="/login"> {/* Cambiado de "../assets/Login.jsx" a "/login" */}
                 <svg className="invert size-14 fixed top-6 right-6 cursor-pointer hover:scale-110 transition-transform" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1} stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                 </svg>
